21741e4ffbe3b926b6b8c643717a3086cfaf8327
suchitra upadhyay <doremon31507@gmail.com>
2025-08-22 10:15:43 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,15 @@
+class Bike
+{
+    Bike(int a)
+	{
+	 System.out.println("parameterzied constructor");
+	 }
+	 
+	 public static void main(String args[])
+	 
+	 {
+	   Bike b=new Bike(15);
+	   }
+}
+	  
+  
